19736d31c9a85fce97566358206015ffced7e5a8
sharfuddinzishan <sharfuddin.cse@gmail.com>
2021-11-13 23:59:27 +0600

    [Completed]

--- src/Component/Dashboard/DashBoardHome/DashBoardHome.js
+++ src/Component/Dashboard/DashBoardHome/DashBoardHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashBoardHome = () => {
     return (
         <>
-            <h1>DashBoard Home</h1>
+            <h1>Welcome To DashBoard Home ! Please Click On Sidebar Menu TO Visit Panel</h1>
         </>
     );
 };
